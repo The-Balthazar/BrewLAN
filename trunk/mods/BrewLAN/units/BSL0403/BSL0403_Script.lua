822e35cea4fdda4cb846cfa21810220e2509cd39
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-01-25 16:11:53 +0000

    Alpha version Iyadesu added (Sera EXP Engineer)

---
+++ trunk/mods/BrewLAN/units/BSL0403/BSL0403_Script.lua
@@ -0,0 +1,25 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/XSL0303/XSL0303_script.lua
+#**  Author(s):  Dru Staltman, Aaron Lundquist
+#**
+#**  Summary  :  Seraphim Experimental Engineer
+#**
+#****************************************************************************
+
+local SConstructionUnit = import('/lua/seraphimunits.lua').SConstructionUnit
+local SLandUnit = import('/lua/seraphimunits.lua').SLandUnit
+local WeaponsFile = import('/lua/seraphimweapons.lua')
+local SDFAireauBolter = WeaponsFile.SDFAireauBolterWeapon
+local SANUallCavitationTorpedo = WeaponsFile.SANUallCavitationTorpedo
+local EffectUtil = import('/lua/EffectUtilities.lua')
+
+BSL0403 = Class(SConstructionUnit) {
+    Weapons = {
+        Torpedo01 = Class(SANUallCavitationTorpedo) {},
+        LeftTurret = Class(SDFAireauBolter) {},
+        RightTurret = Class(SDFAireauBolter) {},
+    },
+}
+
+TypeClass = BSL0403
