d26d7156eb538093b5c6b9279a608f45b03687ab
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-02-09 13:42:52 +0000

    Failed attempts at Hades added, for Adam to poke around with.

---
+++ mods/BrewLAN/units/BRB2306/BRB2306_Script.lua
@@ -0,0 +1,25 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/URB2301/URB2301_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  Cybran Heavy Gun Tower Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
+local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
+local CybranWeaponsFile = import('/lua/cybranweapons.lua')
+local CDFHeavyMicrowaveLaserGenerator = CybranWeaponsFile.CDFHeavyMicrowaveLaserGenerator
+
+BRB2306 = Class(CStructureUnit) {
+    Weapons = {
+        MainGun = Class(CDFHeavyMicrowaveLaserGenerator) {
+        },
+        Laser = Class(CDFParticleCannonWeapon) {
+            FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
+        },
+    },
+}
+
+TypeClass = BRB2306
