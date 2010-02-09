7811c2f17f4644dfdb7c5ae3f38100cc7baad23c
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-02-09 14:59:39 +0000

    Fixed Hades, updated docs.

--- trunk/mods/BrewLAN/units/BRB2306/BRB2306_Script.lua
+++ trunk/mods/BrewLAN/units/BRB2306/BRB2306_Script.lua
@@ -7,18 +7,14 @@
 #**
 #**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
+
 local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
-local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
-local CDFHeavyMicrowaveLaserGenerator = CybranWeaponsFile.CDFHeavyMicrowaveLaserGenerator
+local CDFHeavyMicrowaveLaserGeneratorCom = CybranWeaponsFile.CDFHeavyMicrowaveLaserGeneratorCom
 
 BRB2306 = Class(CStructureUnit) {
     Weapons = {
-        MainGun = Class(CDFHeavyMicrowaveLaserGenerator) {
-        },
-        Laser = Class(CDFParticleCannonWeapon) {
-            FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
-        },
+        MainGun = Class(CDFHeavyMicrowaveLaserGeneratorCom) {},
     },
 }
 
