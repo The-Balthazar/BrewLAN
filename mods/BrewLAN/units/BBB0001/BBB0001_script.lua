2af334fb25dcd7d65e3ce3467d53e6d0950d49a3
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-02-27 00:41:14 +0000

    Aspis minor fix, Sera T3 PD alpha, Devtest unit.

---
+++ mods/BrewLAN/units/BBB0001/BBB0001_script.lua
@@ -0,0 +1,15 @@
+#****************************************************************************
+#**
+#**  Author(s):  Sean Wheeldon
+#**
+#**  Summary  :  Damage test unit
+#**
+#**  Copyright © 2010 BrewLAN.  All rights reserved.
+#****************************************************************************
+
+local AShieldStructureUnit = import('/lua/aeonunits.lua').AShieldStructureUnit
+
+BBB0001 = Class(AShieldStructureUnit) {}
+
+TypeClass = BBB0001
+
