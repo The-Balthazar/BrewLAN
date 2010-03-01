545341db9dc9abd51e5a6b8ad4b34487f8b4e59a
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-03-01 20:45:51 +0000

    File preparation for multilingual translation.

---
+++ mods/BrewLAN/hook/loc/US/strings_db.lua
@@ -0,0 +1,61 @@
+#Tech 1 aircraft
+Unit_Description_BrewLAN_T1A001="Light Guship. Primary role is base defence. Effective against low-level ground units."
+Unit_Description_BrewLAN_T1A002="Light Guship. Effective against low-level ground units. Equipped with a light transport hook."
+Unit_Description_BrewLAN_T1A003="Light Guship. Primary role is base defence. Effective against low-level ground units."
+Unit_Description_BrewLAN_T1A004="Torpedo bomber. Designed to engage low-level naval units."
+Unit_Description_BrewLAN_T1A004="Torpedo bomber. Designed to engage low-level naval units."
+Unit_Description_BrewLAN_T1A004="Torpedo bomber. Designed to engage low-level naval units."
+Unit_Description_BrewLAN_T1A004="Torpedo bomber. Designed to engage low-level naval units."
+
+#Tech 1 defence units
+Unit_Description_BrewLAN_T1D001="Light stationary artillery. Designed to engage low to mid-level units."
+Unit_Description_BrewLAN_T1D001="Light stationary artillery. Designed to engage low to mid-level units."
+Unit_Description_BrewLAN_T1D001="Light stationary artillery. Designed to engage low to mid-level units."
+Unit_Description_BrewLAN_T1D001="Light stationary artillery. Designed to engage low to mid-level units."
+
+#Tech 1 protection units
+Unit_Description_BrewLAN_T1P001="Generates a protective shield around units and structures within its radius."
+Unit_Description_BrewLAN_T1P002="Generates a protective shield around units and structures within its radius."
+Unit_Description_BrewLAN_T1P003="Generates a protective shield around units and structures within its radius. Upradable twice."
+
+#Tech 1 utility units
+Unit_Description_BrewLAN_T1U001="Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Unit_Description_BrewLAN_T1U001="Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Unit_Description_BrewLAN_T1U001="Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Unit_Description_BrewLAN_T1U001="Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+
+#Tech 2 resource units
+Unit_Description_BrewLAN_T2R001="Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Unit_Description_BrewLAN_T2R001="Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Unit_Description_BrewLAN_T2R001="Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Unit_Description_BrewLAN_T2R001="Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Unit_Description_BrewLAN_T2R002="Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Unit_Description_BrewLAN_T2R002="Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Unit_Description_BrewLAN_T2R002="Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Unit_Description_BrewLAN_T2R002="Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+
+#Tech 2 defence units
+Unit_Description_BrewLAN_T2D001="An all-terrain medium damage proximity mine system. Visible only to Omni."
+
+#Tech 2 protection units
+Unit_Description_BrewLAN_T2D002="Generates a protective shield around units and structures within its radius. Shield can be upgraded three times."
+Unit_Description_BrewLAN_T2D002="Generates a heavy shield around units and structures within its radius."
+
+#Tech 3 aircraft
+Unit_Description_BrewLAN_T3A001="Heavy gunship. Armed with Phasic autoguns and powerfull but short range AA weaponry."
+Unit_Description_BrewLAN_T3A002="Heavy air transport. Armed with heavy anti-air defence, an EMP weapon and a stealth field. Can carry up to 16 units."
+Unit_Description_BrewLAN_T3A003="Heavy air transport. Features 32 transportation clamps, heavy weaponry and a shield generator. Can carry up to 32 units."
+Unit_Description_BrewLAN_T3A004="Heavy air transport. Feaures an all-targetting insta-kill lazor, 3000 unit clams and an engraving of your mother."
+Unit_Description_BrewLAN_T3A005="High-level stealth torpedo bomber. Designed to engage high-level naval units and overwelm torpedo defences with large salvos."
+
+#Tech 3 land units
+Unit_Description_BrewLAN_T3L001="Shielded Siege assault bot. Armed with dual Aire-au Bolters."
+Unit_Description_BrewLAN_T3L002="Heavily armored assault tank. Low rate of fire, designed as a heavy support unit."
+
+#Tech 3 buildings
+Unit_Description_BrewLAN_T3B001="Heavy defensive tower. Attacks land- and sea-based units with a powerful sweeping laser. Does not engage aircraft or submerged units."
+Unit_Description_BrewLAN_T3B002="Satellite-based observation system. Has a wide line-of-sight and omni censors. If its control center is destroyed, the satelite is immediately disabled."
+
+#Experimentals
+Unit_Description_BrewLAN_T4B001="Slightly broken experimental point defence. Effectively an immobile fatboy. Wont fire westward."
+Unit_Description_BrewLAN_T4B002="Experimental rapid-fire artillery. Consumes massive amounts of Energy with each shot."
--- mods/BrewLAN/hook/lua/ui/help/unitdescription.lua
+++ mods/BrewLAN/hook/lua/ui/help/unitdescription.lua
@@ -1,40 +1,61 @@
-Description['beb3303'] = "Satellite-based observation system. Has a wide line-of-sight and omni censors. If its control center is destroyed, the satelite is immediately disabled."
-Description['urb4204'] = "Generates a protective shield around units and structures within its radius. Shield can be upgraded three times."
-Description['urb4206'] = "Generates a heavy shield around units and structures within its radius."
-Description['baa0105'] = "Light Guship. Primary role is base defence. Effective against low-level ground units."
-Description['bea0105'] = "Light Guship. Effective against low-level ground units. Equipped with a light transport hook."
-Description['bsa0105'] = "Light Guship. Primary role is base defence. Effective against low-level ground units."
-Description['baa0108'] = "Torpedo bomber. Designed to engage low-level naval units."
-Description['bea0108'] = "Torpedo bomber. Designed to engage low-level naval units."
-Description['bra0108'] = "Torpedo bomber. Designed to engage low-level naval units."
-Description['bsa0108'] = "Torpedo bomber. Designed to engage low-level naval units."
-Description['bab4102'] = "Generates a protective shield around units and structures within its radius."
-Description['beb4102'] = "Generates a protective shield around units and structures within its radius."
-Description['bsb4102'] = "Generates a protective shield around units and structures within its radius. Upradable twice."
-Description['beb1206'] = "Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
-Description['bab1206'] = "Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
-Description['brb1206'] = "Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
-Description['bsb1206'] = "Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
-Description['beb1205'] = "Stores large amounts of energy. Construct next to power generators for adjacency bonus."
-Description['bab1205'] = "Stores large amounts of energy. Construct next to power generators for adjacency bonus."
-Description['brb1205'] = "Stores large amounts of energy. Construct next to power generators for adjacency bonus."
-Description['bsb1205'] = "Stores large amounts of energy. Construct next to power generators for adjacency bonus."
-Description['beb2103'] = "Light stationary artillery. Designed to engage low to mid-level units."
-Description['bab2103'] = "Light stationary artillery. Designed to engage low to mid-level units."
-Description['brb2103'] = "Light stationary artillery. Designed to engage low to mid-level units."
-Description['bsb2103'] = "Light stationary artillery. Designed to engage low to mid-level units."
-Description['bra0306'] = "Heavy air transport. Armed with heavy anti-air defence, an EMP weapon and a stealth field. Can carry up to 16 units."
-Description['bsa0306'] = "Heavy air transport. Features 32 transportation clamps, heavy weaponry and a shield generator. Can carry up to 32 units."
-Description['baa0306'] = "Heavy air transport. Feaures an all-targetting insta-kill lazor, 3000 unit clams and an engraving of your mother."
-Description['bsa0305'] = "Heavy gunship. Armed with Phasic autoguns and powerfull but short range AA weaponry."
-Description['beb2404'] = "Slightly broken experimental point defence. Effectively an immobile fatboy. Wont fire westward."
-Description['bal0311'] = "Heavily armored assault tank. Low rate of fire, designed as a heavy support unit."
-Description['bab5104'] = "Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
-Description['beb5104'] = "Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
-Description['brb5104'] = "Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
-Description['bsb5104'] = "Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
-Description['brb2401'] = "Experimental, rapid-fire artillery. Consumes massive amounts of Energy with each shot."
-Description['beb2220'] = "An all-terrain medium damage proximity mine system. Visible only to Omni."
-Description['bra0307'] = "High-level stealth torpedo bomber. Designed to engage high-level naval units and overwelm torpedo defences with large salvos."
-Description['bsl0311'] = "Shielded Siege assault bot. Armed with dual Aire-au Bolters."
-Description['brb2306'] = "Heavy defensive tower. Attacks land- and sea-based units with a powerful sweeping laser. Does not engage aircraft or submerged units."
+#Tech 1 aircraft
+Description['baa0105'] = "<LOC Unit_Description_BrewLAN_T1A001>Light Guship. Primary role is base defence. Effective against low-level ground units."
+Description['bea0105'] = "<LOC Unit_Description_BrewLAN_T1A002>Light Guship. Effective against low-level ground units. Equipped with a light transport hook."
+Description['bsa0105'] = "<LOC Unit_Description_BrewLAN_T1A003>Light Guship. Primary role is base defence. Effective against low-level ground units."
+Description['baa0108'] = "<LOC Unit_Description_BrewLAN_T1A004>Torpedo bomber. Designed to engage low-level naval units."
+Description['bea0108'] = "<LOC Unit_Description_BrewLAN_T1A004>Torpedo bomber. Designed to engage low-level naval units."
+Description['bra0108'] = "<LOC Unit_Description_BrewLAN_T1A004>Torpedo bomber. Designed to engage low-level naval units."
+Description['bsa0108'] = "<LOC Unit_Description_BrewLAN_T1A004>Torpedo bomber. Designed to engage low-level naval units."
+
+#Tech 1 defence units
+Description['beb2103'] = "<LOC Unit_Description_BrewLAN_T1D001>Light stationary artillery. Designed to engage low to mid-level units."
+Description['bab2103'] = "<LOC Unit_Description_BrewLAN_T1D001>Light stationary artillery. Designed to engage low to mid-level units."
+Description['brb2103'] = "<LOC Unit_Description_BrewLAN_T1D001>Light stationary artillery. Designed to engage low to mid-level units."
+Description['bsb2103'] = "<LOC Unit_Description_BrewLAN_T1D001>Light stationary artillery. Designed to engage low to mid-level units."
+
+#Tech 1 protection units
+Description['bab4102'] = "<LOC Unit_Description_BrewLAN_T1P001>Generates a protective shield around units and structures within its radius."
+Description['beb4102'] = "<LOC Unit_Description_BrewLAN_T1P002>Generates a protective shield around units and structures within its radius."
+Description['bsb4102'] = "<LOC Unit_Description_BrewLAN_T1P003>Generates a protective shield around units and structures within its radius. Upradable twice."
+
+#Tech 1 utility units
+Description['bab5104'] = "<LOC Unit_Description_BrewLAN_T1U001>Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Description['beb5104'] = "<LOC Unit_Description_BrewLAN_T1U001>Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Description['brb5104'] = "<LOC Unit_Description_BrewLAN_T1U001>Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+Description['bsb5104'] = "<LOC Unit_Description_BrewLAN_T1U001>Light air staging facility. Repairs and refuels aircraft small aircraft, one landing pad."
+
+#Tech 2 resource units
+Description['beb1205'] = "<LOC Unit_Description_BrewLAN_T2R001>Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Description['bab1205'] = "<LOC Unit_Description_BrewLAN_T2R001>Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Description['brb1205'] = "<LOC Unit_Description_BrewLAN_T2R001>Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Description['bsb1205'] = "<LOC Unit_Description_BrewLAN_T2R001>Stores large amounts of energy. Construct next to power generators for adjacency bonus."
+Description['beb1206'] = "<LOC Unit_Description_BrewLAN_T2R002>Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Description['bab1206'] = "<LOC Unit_Description_BrewLAN_T2R002>Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Description['brb1206'] = "<LOC Unit_Description_BrewLAN_T2R002>Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+Description['bsb1206'] = "<LOC Unit_Description_BrewLAN_T2R002>Stores large amounts of mass. Construct next to extractors or fabricators for adjacency bonus."
+
+#Tech 2 defence units
+Description['beb2220'] = "<LOC Unit_Description_BrewLAN_T2D001>An all-terrain medium damage proximity mine system. Visible only to Omni."
+
+#Tech 2 protection units
+Description['urb4204'] = "<LOC Unit_Description_BrewLAN_T2D002>Generates a protective shield around units and structures within its radius. Shield can be upgraded three times."
+Description['urb4206'] = "<LOC Unit_Description_BrewLAN_T2D002>Generates a heavy shield around units and structures within its radius."
+
+#Tech 3 aircraft
+Description['bsa0305'] = "<LOC Unit_Description_BrewLAN_T3A001>Heavy gunship. Armed with Phasic autoguns and powerfull but short range AA weaponry."
+Description['bra0306'] = "<LOC Unit_Description_BrewLAN_T3A002>Heavy air transport. Armed with heavy anti-air defence, an EMP weapon and a stealth field. Can carry up to 16 units."
+Description['bsa0306'] = "<LOC Unit_Description_BrewLAN_T3A003>Heavy air transport. Features 32 transportation clamps, heavy weaponry and a shield generator. Can carry up to 32 units."
+Description['baa0306'] = "<LOC Unit_Description_BrewLAN_T3A004>Heavy air transport. Feaures an all-targetting insta-kill lazor, 3000 unit clams and an engraving of your mother."
+Description['bra0307'] = "<LOC Unit_Description_BrewLAN_T3A005>High-level stealth torpedo bomber. Designed to engage high-level naval units and overwelm torpedo defences with large salvos."
+
+#Tech 3 land units
+Description['bsl0311'] = "<LOC Unit_Description_BrewLAN_T3L001>Shielded Siege assault bot. Armed with dual Aire-au Bolters."
+Description['bal0311'] = "<LOC Unit_Description_BrewLAN_T3L002>Heavily armored assault tank. Low rate of fire, designed as a heavy support unit."
+
+#Tech 3 buildings
+Description['brb2306'] = "<LOC Unit_Description_BrewLAN_T3B001>Heavy defensive tower. Attacks land- and sea-based units with a powerful sweeping laser. Does not engage aircraft or submerged units."
+Description['beb3303'] = "<LOC Unit_Description_BrewLAN_T3B002>Satellite-based observation system. Has a wide line-of-sight and omni censors. If its control center is destroyed, the satelite is immediately disabled."
+
+#Experimentals
+Description['beb2404'] = "<LOC Unit_Description_BrewLAN_T4B001>Slightly broken experimental point defence. Effectively an immobile fatboy. Wont fire westward."
+Description['brb2401'] = "<LOC Unit_Description_BrewLAN_T4B002>Experimental rapid-fire artillery. Consumes massive amounts of Energy with each shot."
