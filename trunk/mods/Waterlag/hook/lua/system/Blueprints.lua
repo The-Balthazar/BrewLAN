665ce5c9dbb6fb549e0a28a8236c150cf1e34c42
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-03-04 01:12:15 +0000

    Added additional units to the list.

--- trunk/mods/Waterlag/hook/lua/system/Blueprints.lua
+++ trunk/mods/Waterlag/hook/lua/system/Blueprints.lua
@@ -47,6 +47,29 @@ function WaterlagBasicAmphib(all_bps)
         all_bps['ueb2306'],
         all_bps['uab2306'],
         all_bps['urb2306'],
+#-=-=-=-=-=-=-=-# Walls             #-=-=-=-=-=-=-=-#
+        all_bps['uab5101'],
+        all_bps['ueb5101'],
+        all_bps['urb5101'],
+        all_bps['xsb5101'],
+#-=-=-=-=-=-=-=-# Cybran Shields    #-=-=-=-=-=-=-=-#
+        all_bps['urb4202'],
+        all_bps['urb4204'],
+        all_bps['urb4205'],
+        all_bps['urb4206'],
+        all_bps['urb4207'],
+#-=-=-=-=-=-=-=-# UEF Shields       #-=-=-=-=-=-=-=-#
+#        all_bps['beb4102'],
+        all_bps['ueb4202'],
+        all_bps['ueb4301'],
+#-=-=-=-=-=-=-=-# Aeon Shields      #-=-=-=-=-=-=-=-#
+#        all_bps['bab4102'],
+        all_bps['uab4202'],
+        all_bps['uab4301'],
+#-=-=-=-=-=-=-=-# Seraphim Shields  #-=-=-=-=-=-=-=-#
+        all_bps['bsb4102'],
+        all_bps['xsb4202'],
+        all_bps['xsb4301'],
     }
 
     for arrayIndex, bp in WaterlagBasicUnits do
