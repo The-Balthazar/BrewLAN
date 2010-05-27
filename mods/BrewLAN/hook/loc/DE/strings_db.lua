178b282c52663e384d3d602a4229955f9803963d
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-05-27 13:18:25 +0000

    Now compatible with German versions of SUPCOM:FA.

---
+++ mods/BrewLAN/hook/loc/DE/strings_db.lua
@@ -0,0 +1,115 @@
+﻿## T1
+
+
+bair0105="Leichtes Kanonenboot"
+baa0105_name="Respirer"
+bea0105_name="Pigeon"
+bsa0105_name="Vulesel"
+Unit_Description_BrewLAN_T1A001="Leichtes Kanonenboot. Dient hauptsächlich der Verteidigung. Wirkungsvoll gegen einfache Bodeneinheiten."
+Unit_Description_BrewLAN_T1A002="Leichtes Kanonenboot. Wirkungsvoll gegen einfache Bodeneinheiten. Kann 1 Einheit transportieren."
+Unit_Description_BrewLAN_T1A003="Leichtes Kanonenboot. Dient hauptsächlich der Verteidigung. Wirkungsvoll gegen einfache Bodeneinheiten."
+
+bair0106="Leichtes Torpedobomber"
+baa0106_name="Saksinokka"
+bea0106_name="Seagull"
+bra0106_name="Gannet"
+bsa0106_name="Sou-ioz"
+Unit_Description_BrewLAN_T1A004="Leichtes Torpedobomber. Wirkungsvoll gegen einfache Marineeinheiten."
+
+bbui2103="Leichtes Artilleriestellung"
+bab2103_name="Charis"
+beb2103_name="Poker"
+brb2103_name="Kleine Bertha"
+bsb2103_name="Thaam-atha"
+Unit_Description_BrewLAN_T1D001="Leichte Artilleriestellung. Wirkungsvoll gegen einfache Einheiten."
+
+bbui4102="Leichtes Schildgenerator"
+bab4102_name="Aspis"
+beb4102_name="LSD - Puls"
+bsb4102_name="Atha-istle"
+Unit_Description_BrewLAN_T1P001="Erzeugt einen schützenden Schild um Einheiten und Gebäude innerhalb des Radius."
+Unit_Description_BrewLAN_T1P002="Erzeugt einen schützenden Schild um Einheiten und Gebäude innerhalb des Radius."
+Unit_Description_BrewLAN_T1P003="Erzeugt einen schützenden Schild um Einheiten und Gebäude innerhalb des Radius. Kann zweimal erweitert werden."
+
+bbui5104="Start- und Landeeinrichtung"
+bbuisera5104="Luftstützpunkt"
+bsb5104_name="Iathu-uhthe"
+bab5104_name="Pedestal"
+beb5104_name="Resupply"
+brb5104_name="Ravitailler"
+Unit_Description_BrewLAN_T1U001="Luftstufung Anlage. Reparaturen und Flugzeuge betankt kleine Flugzeuge, ein Landeplatz"
+
+
+## T2
+
+
+### Mass and energy storages use the default strings of their T1 counterparts, except the UEF T2 which has a name.
+beb1206_name="CJ-00F4T-2"
+Unit_Description_BrewLAN_T2R002="Speichert große Mengen an Masse. Konstrukt neben Auszieher oder Verarbeiter für adjacency Bonus."
+Unit_Description_BrewLAN_T2R001="Stores große Mengen von Energie. Konstrukt neben Energiegenerator für adjacency Bonus."
+
+bbui2220="Proximity Mine"
+beb2220_name="AM-12 Anti-Armour"
+Unit_Description_BrewLAN_T2D001="Ein All-Terrain mittlerer Schaden Nähe Mine System. Sichtbar nur für Omnisensorausrüstung."
+
+### Cybran T2 shields; they didn't have build description originally.
+Unit_Description_BrewLAN_T2D002="Erzeugt einen schützenden Schild um Einheiten und Gebäude innerhalb des Radius. Nachgerüstet werden kann, dreimal."
+Unit_Description_BrewLAN_T2D002="Erzeugt einen schützenden Schild um Einheiten und Gebäude innerhalb des Radius."
+
+## T3
+
+
+bair0305="Schweres Kanonenboot"
+bsa0305_name="Vulthuum"
+Unit_Description_BrewLAN_T3A001="Schwerer Kanonenboot. Bewaffnet mit Phasische autoguns und leistungsfähige, aber mit kurzer Reichweite Anti-Luft-Bewaffnung."
+
+bair0306="Schwerer Lufttransporter"
+baa0306_name="Cirrus"
+bra0306_name="Nacht Skimmer"
+bsa0306_name="Vishuum"
+Unit_Description_BrewLAN_T3A002="Schwerer Lufttransporter. Bewaffnet mit schweren anti-Luftverteidigung, eine EMP Waffe und ein Stealth-Bereich. Kann bis zu 16 Einheiten."
+Unit_Description_BrewLAN_T3A003="Schwerer Lufttransporter. Bietet 32 Transporthaken, schwere Kanonen, Raketenwerfer und einen Schildgenerator. Kann bis zu 32 Einheiten transportieren."
+Unit_Description_BrewLAN_T3A004="Schwerer Lufttransporter. Gegeben werden, sobald eine angemessene Beschreibung ihrer beendet."
+
+#bair0307="Schweres Torpedobomber"
+bra0307_name="Zenith"
+Unit_Description_BrewLAN_T3A005="Stealth Torpedobomber. Der Lage ist, mächtige Marineeinheiten."
+
+bal0311_desc="Belagerungspanzer"
+bal0311_name="Moldavite"
+Unit_Description_BrewLAN_T3L002="Belagerungspanzer. Niedrige Rate des Feuers, als ein schweres Unterstützung Einheit konzipiert."
+
+bsl0311_desc="Schwerer Angriffsbot"
+bsl0311_name="Ilshatha"
+Unit_Description_BrewLAN_T3L001="Schwerer Angriffsbot. Ausgestattet mit mächtigen Waffen und ein Schildgenerator."
+
+brb2306_name="Hades"
+bsb2306_name="Othuushala"
+Unit_Description_BrewLAN_T3B001="Heavy Wehrturm. Angriffe Boden und Meer Einheiten mit einem leistungsstarken Laser Kehren. Keine operativen Flugzeugen oder getauchten Einheiten."
+
+
+beb3303_desc="Beobachtung Satellit System"
+bea0002_desc="Beobachtung Satellit"
+bea0002_name="Burning Star"
+Unit_Description_BrewLAN_T3B002="Satelliten-basierte Beobachtung System. Hat eine breite Sichtlinie und Omnisensorausrüstung. Wenn seine Leitstelle zerstört wird, wird der Satellit sofort deaktiviert."
+
+
+## T4
+
+
+bsl0403_desc="Experimentelles Techniker"
+bsl0403_help="Experimentelles Techniker"
+bsl0403_name="Iyadesu"
+Unit_Description_BrewLAN_T4L00="Über powered experimentellen Ingenieur; aufbauen kann sich selbst und die Ressourcen zu tun, so in 20 Sekunden."
+
+bea0401_desc="Experimentelles Kanonenboot"
+bea0401_name="AC-500 Centurion"
+Unit_Description_BrewLAN_T4A001="Experimentelles Kanonenboot."
+
+beb2404_desc="Experimentelles Punktverteidigung"
+beb2404_name="Weltvernichtungsmaschine"
+Unit_Description_BrewLAN_T4B001="Leicht gebrochen experimentellen Punkt Verteidigung. Effektiv eine immobile fatboy. Wont Feuer nach Westen."
+
+brb2401_desc="Experimentelles Artilleriestellung"
+brb2401_name="Scathis Mark II"
+Unit_Description_BrewLAN_T4B002="Experimentelle Schnellfeuer-Artillerie. Verbraucht massive Mengen von Energie mit jedem Schuss."
