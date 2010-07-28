1ff07738de33710653299805d9c9df0556036740
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-07-28 08:50:09 +0000

    Fixed and finished 'Wilson' the Cybran T2 Field Engineer.

---
+++ trunk/mods/BrewLAN/units/BRL0209/BRL0209_script.lua
@@ -0,0 +1,56 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/BRL0209/BRL0209_script.lua
+#**  Author(s):  Sean Wheeldon
+#**
+#**  Summary  :  Cybran T2 Combat Engineer Script, Based on the UEF script
+#**
+#****************************************************************************
+
+local CConstructionUnit = import('/lua/cybranunits.lua').CConstructionUnit
+local CDFElectronBolterWeapon = import('/lua/cybranweapons.lua').CDFElectronBolterWeapon
+
+BRL0209 = Class(CConstructionUnit) {
+
+    Weapons = {
+        Bolter = Class(CDFElectronBolterWeapon) {},
+    },
+
+    Treads = {
+        ScrollTreads = true,
+        BoneName = 'BRL0209',
+        TreadMarks = 'tank_treads_albedo',
+        TreadMarksSizeX = 0.65,
+        TreadMarksSizeZ = 0.4,
+        TreadMarksInterval = 0.3,
+        TreadOffset = { 0, 0, 0 },
+    },
+
+    OnStopBeingBuilt = function(self)
+        self:SetMaintenanceConsumptionActive()
+        CConstructionUnit.OnStopBeingBuilt(self)
+    end,
+
+    OnStartBuild = function(self, unitBeingBuilt, order)
+        --Disable the gun while building something
+        self:SetWeaponEnabledByLabel('Bolter', false)
+        CConstructionUnit.OnStartBuild(self, unitBeingBuilt, order)
+    end,
+    
+    OnStopBuild = function(self)
+        --Re-enable the gun after done building
+        self:SetWeaponEnabledByLabel('Bolter', true)
+        CConstructionUnit.OnStopBuild(self)
+    end,
+    
+    OnStartReclaim = function(self, target)
+        CConstructionUnit.OnStartReclaim(self, target)
+        self:SetAllWeaponsEnabled(false)
+    end,
+    
+    OnStopReclaim = function(self, target)
+        CConstructionUnit.OnStopReclaim(self, target)
+        self:SetAllWeaponsEnabled( true)
+    end,
+}
+TypeClass = BRL0209
