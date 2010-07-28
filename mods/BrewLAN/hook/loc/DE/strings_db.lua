38913ccda9cf93c0f329585f90151e8dfe2f77af
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-07-28 09:14:25 +0000

    Unit description for Wilson, will probably need to get the German translation checked.

--- mods/BrewLAN/hook/loc/DE/strings_db.lua
+++ mods/BrewLAN/hook/loc/DE/strings_db.lua
@@ -43,6 +43,10 @@ Unit_Description_BrewLAN_T1U001="Luftstützpunkt. Rerpariert und Betankt kleine 
 ## T2
 
 
+#Field Engineer
+brl0209_name="Wilson"
+Unit_Description_BrewLAN_T2L001="Amphibisches Fahrzeug der Tech-Stufe 2, das bauen, reparieren, erobern und wiederverwerten kann. Bewaffnet mit einer Aufstandswaffe sowie internem Radar und Schläue."
+
 ### Mass and energy storages use the default strings of their T1 counterparts, except the UEF T2 which has a name.
 beb1206_name="CJ-00F4T-2"
 beb1205_name="PW4TH12-ST Kondensator"
