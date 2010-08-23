19a467153b44e09af1df643e8d9579d0fa228374
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2010-08-23 11:35:45 +0000

    PRODUCTBREWLAN cat, cat fixes, moar decoy planes, texturing, balance.

---
+++ mods/BrewLAN/units/BRA0310/BRA0310_script.lua
@@ -0,0 +1,19 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/URA0302/URA0302_script.lua
+#**  Author(s):  David Tomandl
+#**
+#**  Summary  :  Cybran Spy Plane Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
+
+BRA0310 = Class(CAirUnit) {
+    OnStopBeingBuilt = function(self,builder,layer)
+        CAirUnit.OnStopBeingBuilt(self,builder,layer)
+        self:SetMaintenanceConsumptionActive()
+    end,
+}
+TypeClass = BRA0310
