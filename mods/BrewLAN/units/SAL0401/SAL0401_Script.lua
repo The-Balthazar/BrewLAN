c33fa381977f867ad6f38429e345a9ec972ff0ac
The.Balthazar@gmail.com <The.Balthazar@gmail.com@fbcf2804-98e9-11de-bb56-53d3cd047523>
2012-11-24 18:45:16 +0000

    Absolution Changes, test implementation of the pressure mine.

--- mods/BrewLAN/units/SAL0401/SAL0401_Script.lua
+++ mods/BrewLAN/units/SAL0401/SAL0401_Script.lua
@@ -11,10 +11,12 @@
 local ALandUnit = import('/lua/aeonunits.lua').ALandUnit
 local WeaponsFile = import('/lua/aeonweapons.lua')
 local ADFCannonOblivionWeapon = WeaponsFile.ADFCannonOblivionWeapon02
+local SAMElectrumMissileDefense = import ('/lua/seraphimweapons.lua').SAMElectrumMissileDefense
 
 SAL0401 = Class(ALandUnit) {
     Weapons = {
         MainGun = Class(ADFCannonOblivionWeapon) {},
+        AntiMissile = Class(SAMElectrumMissileDefense) {},
     },
 }
 TypeClass = SAL0401
