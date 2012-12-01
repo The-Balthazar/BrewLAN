0c7c577d82a7936732b965028209d6a7081ca306
The.Balthazar@gmail.com <The.Balthazar@gmail.com@fbcf2804-98e9-11de-bb56-53d3cd047523>
2012-12-01 20:26:06 +0000

    Mostly things related to the Aeon T4 Tank.

--- trunk/mods/BrewLAN/units/SAL0401/SAL0401_Script.lua
+++ trunk/mods/BrewLAN/units/SAL0401/SAL0401_Script.lua
@@ -1,11 +1,7 @@
 #****************************************************************************
 #**
-#**  File     :  /cdimage/units/DAL0310/DAL0310_script.lua
-#**  Author(s):  Dru Staltman, Matt Vainio
+#**  Summary  :  Aeon Experimental Siege Tank script
 #**
-#**  Summary  :  Aeon Shield Disruptor Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 
 local ALandUnit = import('/lua/aeonunits.lua').ALandUnit
