8133beaba9038c0fd4a8a40f425fd7705ef245c4
The.Balthazar@gmail.com <The.Balthazar@gmail.com@fbcf2804-98e9-11de-bb56-53d3cd047523>
2012-12-15 22:30:22 +0000

    Added T2 energy storage scaled explosions.

--- mods/BrewLAN/units/SAB1205/SAB1205_script.lua
+++ mods/BrewLAN/units/SAB1205/SAB1205_script.lua
@@ -8,6 +8,8 @@
 #**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 local AEnergyStorageUnit = import('/lua/aeonunits.lua').AEnergyStorageUnit
+local CMobileKamikazeBombWeapon = import('/lua/cybranweapons.lua').CMobileKamikazeBombWeapon
+local EffectTemplate = import('/lua/EffectTemplates.lua')
 
 SAB1205 = Class(AEnergyStorageUnit) {
 
@@ -15,6 +17,63 @@ SAB1205 = Class(AEnergyStorageUnit) {
         AEnergyStorageUnit.OnStopBeingBuilt(self,builder,layer)
         self.Trash:Add(CreateStorageManip(self, 'Side_Pods', 'ENERGY', 0, 0, 0, 0, 0, .3*1.5))
         self.Trash:Add(CreateStorageManip(self, 'Center_Pod', 'ENERGY', 0, 0, 0, 0, 0, .3*1.5))
+    end,
+
+    Weapons = {
+        DeathWeapon0 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsSml01,
+        },
+        DeathWeapon1 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsSml01,
+        },
+        DeathWeapon2 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsMed01,
+        },
+        DeathWeapon3 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsMed01,
+        },
+        DeathWeapon4 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsLrg01,
+        },
+        DeathWeapon5 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsLrg01,
+        },
+        DeathWeapon6 = Class(CMobileKamikazeBombWeapon) {      
+   			FxDeath = EffectTemplate.ExplosionEffectsLrg02,
+        },
+    },
+
+    OnKilled = function(self, instigator, type, overkillRatio)
+
+
+                local bp = self:GetBlueprint()
+                local aiBrain = GetArmyBrain(self:GetArmy())
+                local curEnergy = aiBrain:GetEconomyStoredRatio('ENERGY')
+
+                if self:IsBeingBuilt() then 
+                else
+                    if curEnergy < 0.2 then
+                        self:GetWeaponByLabel('DeathWeapon0'):FireWeapon()
+                        #LOG('Weapon0')
+                    elseif curEnergy < 0.4 then
+                        self:GetWeaponByLabel('DeathWeapon1'):FireWeapon()
+                        #LOG('Weapon1')
+                    elseif curEnergy < 0.6 then
+                        self:GetWeaponByLabel('DeathWeapon2'):FireWeapon()
+                        #LOG('Weapon2')
+                    elseif curEnergy < 0.8 then
+                        self:GetWeaponByLabel('DeathWeapon3'):FireWeapon()
+                        #LOG('Weapon3')
+                    elseif curEnergy < 0.9 then
+                        self:GetWeaponByLabel('DeathWeapon4'):FireWeapon()
+                        #LOG('Weapon4')
+                    elseif curEnergy <= 1.0 then
+                        self:GetWeaponByLabel('DeathWeapon5'):FireWeapon()
+                        #LOG('Weapon5')
+                    end
+                end
+
+        AEnergyStorageUnit.OnKilled(self)
     end,
 
 }
