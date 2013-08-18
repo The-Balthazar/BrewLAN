804b3389f05c9a42a955178d79c7a25852ddb5fd
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2013-08-18 13:19:43 +0000

    Herp de derp I'll just leave this here. (I forgot to tick this for upload last revision)

---
+++ trunk/mods/BrewLAN/units/SRL0320/SRL0320_Script.lua
@@ -0,0 +1,25 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/URL0304/URL0304_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  Cybran Heavy Mobile Artillery Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CLandUnit = import('/lua/cybranunits.lua').CLandUnit
+local CAAMissileNaniteWeapon = import('/lua/cybranweapons.lua').CAAMissileNaniteWeapon
+
+SRL0320 = Class(CLandUnit) {
+    Weapons = {
+        MainGun = Class(CAAMissileNaniteWeapon) {},
+    },
+    OnStopBeingBuilt = function(self,builder,layer)
+        CLandUnit.OnStopBeingBuilt(self,builder,layer)
+        self:SetMaintenanceConsumptionActive()
+        self:EnableUnitIntel('Cloak')
+    end,
+}
+
+TypeClass = SRL0320
