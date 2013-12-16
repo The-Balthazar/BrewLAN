795ad741198efd9654788a04bd2e1807c283da79
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2013-12-16 23:00:05 +0000

    Centurion things.

--- mods/BrewLAN/units/SEA0401/SEA0401_Script.lua
+++ mods/BrewLAN/units/SEA0401/SEA0401_Script.lua
@@ -8,34 +8,20 @@
 #****************************************************************************
 
 local TAirUnit = import('/lua/terranunits.lua').TAirUnit
-local WeaponsFile = import('/lua/terranweapons.lua')
-local TIFSmallYieldNuclearBombWeapon = WeaponsFile.TIFSmallYieldNuclearBombWeapon
-local TAirToAirLinkedRailgun = WeaponsFile.TAirToAirLinkedRailgun
-local TSAMLauncher = WeaponsFile.TSAMLauncher
-local TDFHeavyPlasmaCannonWeapon = WeaponsFile.TDFHeavyPlasmaCannonWeapon
-local TDFGaussCannonWeapon = WeaponsFile.TDFGaussCannonWeapon
-
+local TAAFlakArtilleryCannon = import('/lua/terranweapons.lua').TAAFlakArtilleryCannon
 local EffectUtil = import('/lua/EffectUtilities.lua')
 local CreateUEFBuildSliceBeams = EffectUtil.CreateUEFBuildSliceBeams
 
 SEA0401 = Class(TAirUnit) {
-    BeamExhaustCruise = '/effects/emitters/missile_exhaust_fire_beam_03_emit.bp',
+    BeamExhaustCruise = '/mods/BrewLAN/effects/emitters/brewlan_missile_exhaust_fire_beam_01_emit.bp',
 
-    EngineRotateBones = {'Engine_Body001', 'Engine_Body002',},
+    EngineRotateBones = {'Engine_Body001', 'Engine_Body002','Engine_Body003', 'Engine_Body004',},
 
     Weapons = {
-        GaussTurret01 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        GaussTurret02 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        GaussTurret03 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        GaussTurret04 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        GaussTurret05 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        GaussTurret06 = Class(TDFHeavyPlasmaCannonWeapon) {},
-        FrontTurret01 = Class(TDFGaussCannonWeapon) {},
-        FrontTurret02 = Class(TDFGaussCannonWeapon) {},
-        LinkedRailGun1 = Class(TAirToAirLinkedRailgun) {},
-        LinkedRailGun2 = Class(TAirToAirLinkedRailgun) {},
-        MissileRack01 = Class(TSAMLauncher) {},
+        HeadAAGun = Class(TAAFlakArtilleryCannon) {},
+        RearAAGun = Class(TAAFlakArtilleryCannon) {},
     },
+
     DestructionPartsChassisToss = {'SEA0401',},
     DestroyNoFallRandomChance = 1.1,
 
@@ -51,17 +37,17 @@ SEA0401 = Class(TAirUnit) {
         # set up the thursting arcs for the engines
         for key,value in self.EngineManipulators do
             #                          XMAX, XMIN, YMAX,YMIN, ZMAX,ZMIN, TURNMULT, TURNSPEED
-            value:SetThrustingParam( -0.0, 0.0, -0.25, 0.25, -0.1, 0.1, 1.0,      0.25 )
+            value:SetThrustingParam( -0.05, 0.05, -0.25, 0.25, -0.1, 0.1, 1.0,      0.25 )
         end
         
         for k, v in self.EngineManipulators do
             self.Trash:Add(v)
         end
 
-        self.Rotator1 = CreateRotator(self, 'Engine_Propeller001', 'y', nil, 1000, 100, 1000)
-        self.Rotator2 = CreateRotator(self, 'Engine_Propeller002', 'y', nil, 1000, 100, 1000)
-        self.Rotator3 = CreateRotator(self, 'Engine_Propeller003', 'y', nil, -1000, -100, -1000)
-        self.Rotator4 = CreateRotator(self, 'Engine_Propeller004', 'y', nil, -1000, -100, -1000)
+        self.Rotator1 = CreateRotator(self, 'Engine_Propeller001', 'y', nil, 1000, 250, 1000)
+        self.Rotator2 = CreateRotator(self, 'Engine_Propeller002', 'y', nil, 1000, 250, 1000)
+        self.Rotator3 = CreateRotator(self, 'Engine_Propeller003', 'y', nil, -1000, 250, -1000)
+        self.Rotator4 = CreateRotator(self, 'Engine_Propeller004', 'y', nil, -1000, 250, -1000)
         self.Trash:Add(self.Rotator1)
         self.Trash:Add(self.Rotator2)
         self.Trash:Add(self.Rotator3)
