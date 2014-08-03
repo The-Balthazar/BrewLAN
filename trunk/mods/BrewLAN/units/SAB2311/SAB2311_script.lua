439187b2d731a6f083a44cf2be7c85c3bdf064c7
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-08-03 14:23:02 +0000

    Mobile anti-nuke balance changes, Gantry building Centurion build space fix, NEW GUN.

---
+++ trunk/mods/BrewLAN/units/SAB2311/SAB2311_script.lua
@@ -0,0 +1,19 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UAS0401/UAS0401_script.lua
+#**  Author(s):  John Comes
+#**
+#**  Summary  :  Aeon Experimental Sub
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
+
+SAB2311 = Class(AStructureUnit) {
+    Weapons = {
+        MainGun = Class(import('/lua/aeonweapons.lua').ADFCannonOblivionWeapon02) {},
+    },
+}
+
+TypeClass = SAB2311
