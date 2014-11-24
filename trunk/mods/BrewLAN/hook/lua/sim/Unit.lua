6fefd788a7edc62b5ccf93254fe29930f0b45119
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-11-24 23:50:20 +0000

    Iyadesu is now a rezbot.

--- trunk/mods/BrewLAN/hook/lua/sim/Unit.lua
+++ trunk/mods/BrewLAN/hook/lua/sim/Unit.lua
@@ -8,7 +8,12 @@ Unit = Class(UnitOld) {
                 self:AddCommandCap('RULEUCC_Stop')
                 self.CouldntStop = true
             end
-        end
+        end                                               
+        if order == 'Repair' and unitBeingBuilt.IsRezzedGuy then
+            self.Rezrepairing = true
+        else      
+            self.Rezrepairing = false
+        end       
 		  UnitOld.OnStartBuild(self, unitBeingBuilt, order)
     end,
 
@@ -16,7 +21,10 @@ Unit = Class(UnitOld) {
         if self.CouldntStop then
             self:RemoveCommandCap('RULEUCC_Stop')
             self.CouldntStop = false
-        end  
+        end                      
+        if self.Rezrepairing then
+            unitBeingBuilt.IsRezzedGuy = 0.9 * unitBeingBuilt:GetHealthPercent()
+        end           
         UnitOld.OnStopBuild(self, unitBeingBuilt)
     end,
 
@@ -24,8 +32,71 @@ Unit = Class(UnitOld) {
         if self.CouldntStop then
             self:RemoveCommandCap('RULEUCC_Stop')
             self.CouldntStop = false
-        end  
+        end                       
+        if self.Rezrepairing then
+            unitBeingBuilt.IsRezzedGuy = 0.9 * unitBeingBuilt:GetHealthPercent()
+        end   
         UnitOld.OnFailedToBuild(self)
     end,
+    
+    CreateWreckageProp = function( self, overkillRatio )
+        local bp = self:GetBlueprint()
+        local wreck = bp.Wreckage.Blueprint
+        if wreck then
+            #LOG('*DEBUG: Spawning Wreckage = ', repr(wreck), 'overkill = ',repr(overkillRatio))
+            local pos = self:GetPosition()
+            local mass = bp.Economy.BuildCostMass * (bp.Wreckage.MassMult or 0)
+            local energy = bp.Economy.BuildCostEnergy * (bp.Wreckage.EnergyMult or 0)
+            local time = (bp.Wreckage.ReclaimTimeMultiplier or 1)
+            if self:GetCurrentLayer() == 'Seabed' or self:GetCurrentLayer() == 'Land' then
+                pos[2] = GetTerrainHeight(pos[1], pos[3]) + GetTerrainTypeOffset(pos[1], pos[3])
+            else
+                pos[2] = GetSurfaceHeight(pos[1], pos[3]) + GetTerrainTypeOffset(pos[1], pos[3])
+            end
+            
+            local prop = CreateProp( pos, wreck )
+            
+            # We make sure keep only a bounded list of wreckages around so we don't get into perf issues when
+            # we accumulate too many wreckages
+            prop:AddBoundedProp(mass)
+            
+            prop:SetScale(bp.Display.UniformScale)
+            prop:SetOrientation(self:GetOrientation(), true)
+            prop:SetPropCollision('Box', bp.CollisionOffsetX, bp.CollisionOffsetY, bp.CollisionOffsetZ, bp.SizeX* 0.5, bp.SizeY* 0.5, bp.SizeZ * 0.5)
+            prop:SetMaxReclaimValues(time, time, mass, energy)
+            mass = (mass - (mass * (overkillRatio or 1))) * self:GetFractionComplete()     
+            if self.IsRezzedGuy then
+            mass = mass * self.IsRezzedGuy
+            end
+            energy = (energy - (energy * (overkillRatio or 1))) * self:GetFractionComplete()
+            time = time - (time * (overkillRatio or 1))
+            
+            prop:SetReclaimValues(time, time, mass, energy)
+            prop:SetMaxHealth(bp.Defense.Health)
+            prop:SetHealth(self, bp.Defense.Health * (bp.Wreckage.HealthMult or 1))
+            
+            #FIXME: SetVizToNeurals('Intel') is correct here, so you can't see enemy wreckage appearing
+            # under the fog. However the engine has a bug with prop intel that makes the wreckage
+            # never appear at all, even when you drive up to it, so this is disabled for now.
+            #prop:SetVizToNeutrals('Intel')
+            if not bp.Wreckage.UseCustomMesh then
+            prop:SetMesh(bp.Display.MeshBlueprintWrecked)
+        end
+        
+        # Attempt to copy our animation pose to the prop. Only works if
+        # the mesh and skeletons are the same, but will not produce an error
+        # if not.
+        TryCopyPose(self,prop,false)
+        
+        prop.AssociatedBP = self:GetBlueprint().BlueprintId
+        
+        # Create some ambient wreckage smoke
+        explosion.CreateWreckageEffects(self,prop)
+        prop.OldId = self:GetBlueprint().BlueprintId
+        return prop
+        else
+            return nil
+        end
+    end, 
 }
 
--- trunk/mods/BrewLAN/units/SSL0403/SSL0403_Script.lua
+++ trunk/mods/BrewLAN/units/SSL0403/SSL0403_Script.lua
@@ -23,7 +23,47 @@ SSL0403 = Class(SConstructionUnit) {
     StartBeingBuiltEffects = function(self, builder, layer)
 		SConstructionUnit.StartBeingBuiltEffects(self, builder, layer)
 		self:ForkThread( EffectUtil.CreateSeraphimExperimentalBuildBaseThread, builder, self.OnBeingBuiltEffectsBag )
-    end,    
+    end,     
+          
+    OnStartReclaim = function(self, target)
+		  SConstructionUnit.OnStartReclaim(self, target)
+        self.RezTarget = {
+            id = target.OldId,
+            pos = target:GetPosition(),
+            ori = target:GetOrientation(),
+        }
+        --self:tprint(target:GetBlueprint())
+        --LOG(target.OldId)
+    end,
+
+    OnStopReclaim = function(self, target)      
+		  SConstructionUnit.OnStopReclaim(self, target)         
+        if not target and self.RezTarget.id then
+            local pos = self.RezTarget.pos   
+            local ori = self.RezTarget.ori
+            local rezzedGuy = CreateUnitHPR(self.RezTarget.id, self:GetArmy(), pos[1], pos[2], pos[3], ori[1], ori[2], ori[2])
+            rezzedGuy:SetHealth(self, 1 )
+            rezzedGuy.IsRezzedGuy = 0
+            IssueRepair({self},rezzedGuy)
+        end 
+    end,
+    
+    tprint = function(self, tbl, indent)
+        if not indent then indent = 0 end
+        for k, v in pairs(tbl) do
+            formatting = string.rep("  ", indent) .. k .. ": "
+            if type(v) == "table" then
+                LOG(formatting)
+                self:tprint(v, indent+1)
+            elseif type(v) == 'boolean' then
+                LOG(formatting .. tostring(v))		
+            elseif type(v) == 'string' or type(v) == 'number' then
+                LOG(formatting .. v)
+            else
+                LOG(formatting .. type(v))
+            end
+        end
+    end,
 }
 
 TypeClass = SSL0403
