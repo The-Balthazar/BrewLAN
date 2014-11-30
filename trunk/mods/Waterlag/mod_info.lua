9add3d476005016ee33a04d9a64f1f7c8e84e3a4
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-11-30 14:03:43 +0000

    Restructuring clean up.

--- trunk/mods/Waterlag/mod_info.lua
+++ trunk/mods/Waterlag/mod_info.lua
@@ -1,7 +1,7 @@
 name = "Waterlag"
 uid = "25D57D85-9JA7-D842-GKG4-ASJDKG49G8S50"
 version = "0.5"
-copyright = "2009 Sean Wheeldon"
+copyright = "2009 - 2014 Sean Wheeldon"
 description = "Adds aquatic abilities to each building that isn't a factory, wall, silo, or experimental."
 author = "Sean Wheeldon"
 icon = "/mods/waterlag/mod_icon.dds"
