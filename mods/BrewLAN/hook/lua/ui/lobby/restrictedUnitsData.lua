e63ce686b7d60ba03604ce30bfc72d54debce709
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-12-14 16:10:33 +0000

    Re-textured UEF wall. Particle fix for Seraphim T1 gunship. Added Seraphim T1 Torpedo Bomber. Suthanus (Sera T4 arty) added to game enders list. Iyadesu description changes.

--- mods/BrewLAN/hook/lua/ui/lobby/restrictedUnitsData.lua
+++ mods/BrewLAN/hook/lua/ui/lobby/restrictedUnitsData.lua
@@ -1,18 +1,25 @@
 do
-    table.insert(restrictedUnits.GAMEENDERS.categories, "srb2401")
-    local shields = {
-        --Tech 1 shields
-        "sab4102",
-        "seb4102",
-        "ssb4102",
-        --Cybran shields
-        "urb4204",
-        "urb4205",
-        "urb4206",
-        "urb4207",
-        "srb4401",
-    }
-    for i, v in shields do
-        table.insert(restrictedUnits.BUBBLES.categories, v)
+    local Units = {
+        GAMEENDERS = {
+            "srb2401",
+            "ssb2404",
+        },
+        BUBBLES = {
+            --Tech 1 shields
+            "sab4102",
+            "seb4102",
+            "ssb4102",
+            --Cybran shields
+            "urb4204",
+            "urb4205",
+            "urb4206",
+            "urb4207",
+            "srb4401",
+        },
+    }  
+    for k, v in Units do
+        for i in v do  
+            table.insert(restrictedUnits[k].categories, v[i])
+        end
     end
 end
