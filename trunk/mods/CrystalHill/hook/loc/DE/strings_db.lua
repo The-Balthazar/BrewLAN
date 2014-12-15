17d913e42c786830bf4c8c12e3e638dbbf33bf76
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-12-15 21:26:37 +0000

    Minor BrewUI script change, Crystal Hill German localisation.

---
+++ trunk/mods/CrystalHill/hook/loc/DE/strings_db.lua
@@ -0,0 +1,8 @@
+--Translated by 106.Bluebird
+crystal_0001=" kontrolliert den Kristall."
+crystal_0002="'s Team gewinnt mit dem Kristall."
+crystal_0003="'s Team gewinnt mit dem Kristall."
+crystal_0004=" gewinnt mit dem Kristall."
+
+crystal_0005="10 Minuten verbleibend."
+crystal_0006="2 Minuten und 30 Sekunden verbleibend."
