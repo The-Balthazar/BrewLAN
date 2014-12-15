ce9ecf8279d0dea88b8356fbd3537fc5d1d4d687
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2014-12-15 20:33:01 +0000

    Added an off auto off feature for BrewUI. It no longer does anything without BrewLAN, so you don't need to make sure to turn it off when disabling BrewLAN.

--- mods/BrewLAN_Modules/BrewUI/mod_info.lua
+++ mods/BrewLAN_Modules/BrewUI/mod_info.lua
@@ -1,8 +1,8 @@
-name = "BrewUI 0.1"
-uid = "25D57D85-7D84-27HT-A501-BR3WUI0000001"
-version = "0.1"
+name = "BrewUI 0.2"
+uid = "25D57D85-7D84-27HT-A501-BR3WUI0000002"
+version = 0.2
 copyright = "2009 - 2014 Sean Wheeldon"
-description = "Interface changes for BrewLAN. Dont run without BrewLAN. Will conflict with any mod with a \"/hook/lua/ui/game/avatars.lua\" file."
+description = "BrewLAN engineer tab. Dormant without BrewLAN. May conflict with mods with a \"/hook/lua/ui/game/avatars.lua\" file while BrewLAN is active."
 author = "Sean Wheeldon"
 icon = "/mods/BrewLAN_Modules/BrewUI/mod_icon.dds"
 selectable = true
