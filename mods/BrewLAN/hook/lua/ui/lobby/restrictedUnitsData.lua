4c4b968f3ad408b9ed7ec896896b9daca2114398
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2015-01-03 00:50:36 +0000

    Restricted units.

--- mods/BrewLAN/hook/lua/ui/lobby/restrictedUnitsData.lua
+++ mods/BrewLAN/hook/lua/ui/lobby/restrictedUnitsData.lua
@@ -16,10 +16,24 @@ do
             "urb4207",
             "srb4401",
         },
+        NUKE = {
+            --Nuke Mines
+            "srb2222",
+            "seb2222",
+            "sab2222",
+            "ssb2222",
+            "sal0321",
+            "sel0321",
+            "srl0321",
+            "ssl0321",
+        },
     }  
     for k, v in Units do
         for i in v do  
             table.insert(restrictedUnits[k].categories, v[i])
         end
+    end
+    if not categories.deb4303 then
+        table.removeByValue(restrictedUnits.BUBBLES.categories, "deb4303")
     end
 end
