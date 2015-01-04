ce65821452c5c689c08d1a6a50195289b610c329
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2015-01-04 06:42:50 +0000

    Fixed the Suthanus, and the crash without Sorian AI.

--- trunk/mods/BrewLAN/hook/lua/platoon.lua
+++ trunk/mods/BrewLAN/hook/lua/platoon.lua
@@ -1,36 +1,39 @@
-do
-local SUtils = import('/lua/AI/sorianutilities.lua')
-sorianoldPlatoon = Platoon
-
-Platoon = Class(sorianoldPlatoon) {
-
-    ExperimentalAIHubSorian = function(self)
+do  
+    if Platoon.ExperimentalAIHubSorian then
+        local SUtils = import('/lua/AI/sorianutilities.lua')
         
-        local aiBrain = self:GetBrain()
-        local behaviors = import('/lua/ai/AIBehaviors.lua')
+        sorianoldPlatoon = Platoon
         
-        local experimental = self:GetPlatoonUnits()[1]
-        if not experimental then
-            return
-        end
-        if Random(1,5) == 3 and (not aiBrain.LastTaunt or GetGameTimeSeconds() - aiBrain.LastTaunt > 90) then
-            local randelay = Random(60,180)
-            aiBrain.LastTaunt = GetGameTimeSeconds() + randelay
-            SUtils.AIDelayChat('enemies', ArmyBrains[aiBrain:GetArmyIndex()].Nickname, 't4taunt', nil, randelay)
-        end
-        local ID = experimental:GetUnitId()
+        Platoon = Class(sorianoldPlatoon) {
         
-        self:SetPlatoonFormationOverride('AttackFormation')
-        
-        if ID == 'sal0401' then              
-            LOG("Absolution pretending to be a Fatboy innitiated")
-            return behaviors.FatBoyBehaviorSorian(self)
-        elseif ID == 'sea0401' then                  
-            LOG("Centurion behaviour script innitiated")
-            return behaviors.CenturionBehaviorBrewLAN(self)
-        end
-        
-        sorianoldPlatoon.ExperimentalAIHubSorian(self)
-    end,
-}
+            ExperimentalAIHubSorian = function(self)
+                
+                local aiBrain = self:GetBrain()
+                local behaviors = import('/lua/ai/AIBehaviors.lua')
+                
+                local experimental = self:GetPlatoonUnits()[1]
+                if not experimental then
+                    return
+                end
+                if Random(1,5) == 3 and (not aiBrain.LastTaunt or GetGameTimeSeconds() - aiBrain.LastTaunt > 90) then
+                    local randelay = Random(60,180)
+                    aiBrain.LastTaunt = GetGameTimeSeconds() + randelay
+                    SUtils.AIDelayChat('enemies', ArmyBrains[aiBrain:GetArmyIndex()].Nickname, 't4taunt', nil, randelay)
+                end
+                local ID = experimental:GetUnitId()
+                
+                self:SetPlatoonFormationOverride('AttackFormation')
+                
+                if ID == 'sal0401' then              
+                    LOG("Absolution pretending to be a Fatboy innitiated")
+                    return behaviors.FatBoyBehaviorSorian(self)
+                elseif ID == 'sea0401' then                  
+                    LOG("Centurion behaviour script innitiated")
+                    return behaviors.CenturionBehaviorBrewLAN(self)
+                end
+                
+                sorianoldPlatoon.ExperimentalAIHubSorian(self)
+            end,
+        } 
+    end
 end
