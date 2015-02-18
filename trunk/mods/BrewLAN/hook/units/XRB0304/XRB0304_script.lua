af88da8b4ee4cb4d40bfc11b6c4489f2473c0fb1
The.Balthazar <The.Balthazar@fbcf2804-98e9-11de-bb56-53d3cd047523>
2015-02-18 19:07:04 +0000

    Fixed the missing hooked script for the Hive.

---
+++ trunk/mods/BrewLAN/hook/units/XRB0304/XRB0304_script.lua
@@ -0,0 +1,22 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/XRB0304/XRB0304_script.lua
+#**  Author(s):  Dru Staltman, Gordon Duclos
+#**
+#**  Summary  :  Cybran Engineering tower
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local CConstructionStructureUnitBase = import('/lua/cybranunits.lua').CConstructionStructureUnit
+local CConstructionStructureUnit = XRB0304
+XRB0304 = Class(CConstructionStructureUnit) 
+{
+    OnStartBeingBuilt = function(self, builder, layer)
+        if builder:GetBlueprint().BlueprintId == 'xrb0204' then
+            CConstructionStructureUnit.OnStartBeingBuilt(self, builder, layer)
+        else 
+            CConstructionStructureUnitBase.OnStartBeingBuilt(self, builder, layer)
+        end
+    end,
+}
+TypeClass = XRB0304
