da928c6bebd320d39250820d3e38879062ee53d9
The-Balthazar <the.balthazar@gmail.com>
2015-05-31 20:23:51 +0100

    Temporary wall thing.

---
+++ mods/BrewLAN/units/SEB5210/SEB5210_script.lua
@@ -0,0 +1,11 @@
+#****************************************************************************
+#** 
+#**  Cybran Wall: With cordinal scripting
+#** 
+#****************************************************************************
+
+local CardinalWallScript = import('/mods/brewlan/lua/defaultunits.lua').CardinalWallScript
+
+SEB5210 = Class(CardinalWallScript) {}
+
+TypeClass = SEB5210
