ea2cf050963fd8f7dbbc36890f294e76a602bd28
The-Balthazar <the.balthazar@gmail.com>
2015-06-21 14:43:15 +0100

    Waterlag water legs. Cybran ones.

--- mods/Waterlag/hook/lua/sim/Unit.lua
+++ mods/Waterlag/hook/lua/sim/Unit.lua
@@ -26,8 +26,14 @@ Unit = Class(UnitOld) {
                 else
                     LEGS(self, 'UEF_SIZE4_Floatation', 0.083)
                 end
-            elseif EntityCategoryContains(categories.CYBRAN, self) then     
-                LEGS(self, 'CYBRAN_SIZE4_Floatation', 0.1)
+            elseif EntityCategoryContains(categories.CYBRAN, self) then  
+                if self:GetBlueprint().Footprint.SizeX >= 5 and self:GetBlueprint().Footprint.SizeZ >= 5 then 
+                    LEGS(self, 'CYBRAN_SIZE16_Floatation', 0.1)   
+                elseif self:GetBlueprint().Footprint.SizeX >= 3 and self:GetBlueprint().Footprint.SizeZ >= 3 then
+                    LEGS(self, 'CYBRAN_SIZE12_Floatation', 0.1)
+                else
+                    LEGS(self, 'CYBRAN_SIZE4_Floatation', 0.1)
+                end
             elseif EntityCategoryContains(categories.AEON, self) then
                 --SOMETHING
             elseif EntityCategoryContains(categories.SERAPHIM, self) then
