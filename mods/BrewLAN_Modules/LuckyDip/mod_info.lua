6a3eec8bcc77ac9d84d04c857008942e116be69e
The-Balthazar <the.balthazar@gmail.com>
2015-07-03 22:21:54 +0100

    Waterlag partial revert. LuckyDip sub-mod added.

---
+++ mods/BrewLAN_Modules/LuckyDip/mod_info.lua
@@ -0,0 +1,18 @@
+name = "Lucky Dip"
+uid = "25D57D85-7D84-27HT-A502-LDIPS0000001"
+version = 0.1
+copyright = "2015 Sean Wheeldon"
+description = "BlackOps/BrewLAN unit lucky dip. Who knows what units you will get."
+author = "Sean Wheeldon"       
+requires = { 
+    "9e8ea941-c306-4751-b367-a11000000600",
+    "25D57D85-7D84-27HT-A501-BR3WL4N000060",
+}
+requiresNames = {
+    ["9e8ea941-c306-4751-b367-a11000000600"] = "BlackOps 6",
+    ["25D57D85-7D84-27HT-A501-BR3WL4N000060"] = "BrewLAN 0.6",
+}
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
--- mods/Waterlag/hook/lua/system/Blueprints.lua
+++ mods/Waterlag/hook/lua/system/Blueprints.lua
@@ -42,8 +42,8 @@ function Waterlag(all_bps)
                             bp.Wreckage.WreckageLayers.Water = true
                         end
                         table.insert(bp.Categories, 'GIVEMELEGS')
-                        bp.CollisionOffsetY = (bp.CollisionOffsetY or 0) - 2
-                        if bp.Physics then
+                        bp.CollisionOffsetY = (bp.CollisionOffsetY or 0) - .5
+                        --[[if bp.Physics then
                             bp.Physics.MeshExtentsOffsetY = (bp.Physics.MeshExtentsOffsetY or 0) -2
                             bp.Physics.MeshExtentsY = (bp.Physics.MeshExtentsOffsetY or 0) + 2
                         else
@@ -51,7 +51,7 @@ function Waterlag(all_bps)
                                 MeshExtentsOffsetY = - 2,
                                 MeshExtentsY = 2,
                             }
-                        end
+                        end]]--
                     end
                 end
             end
