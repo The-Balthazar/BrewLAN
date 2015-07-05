487ebba019cf0449ab11367dd6daa3d609c62994
The-Balthazar <the.balthazar@gmail.com>
2015-07-05 20:51:21 +0100

    Removing restrictions from lucky dip.

--- mods/BrewLAN_Modules/LuckyDip/mod_info.lua
+++ mods/BrewLAN_Modules/LuckyDip/mod_info.lua
@@ -4,14 +4,14 @@ version = 0.1
 copyright = "2015 Sean Wheeldon"
 description = "BlackOps/BrewLAN unit lucky dip. Who knows what units you will get."
 author = "Sean Wheeldon"       
-requires = { 
+--[[requires = { 
     "9e8ea941-c306-4751-b367-a11000000600",
     "25D57D85-7D84-27HT-A501-BR3WL4N000060",
 }
 requiresNames = {
     ["9e8ea941-c306-4751-b367-a11000000600"] = "BlackOps 6",
     ["25D57D85-7D84-27HT-A501-BR3WL4N000060"] = "BrewLAN 0.6",
-}
+}]]--
 selectable = true
 enabled = true
 exclusive = false
