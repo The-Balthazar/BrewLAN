edfbebe5dcee1e1b6f44a712a55973c4390785b6
The-Balthazar <the.balthazar@gmail.com>
2016-11-05 13:32:14 +0000

    Separated out AI for testing purposes.

---
+++ mods/BrewLAN_Modules/BrewAI/mod_info.lua
@@ -0,0 +1,16 @@
+name = "BrewLAN: Sorian AI"
+uid = "25D57D85-7D84-27HT-A501-BR3WAI0000001"
+version = 0.1
+copyright = "© 2016 - 2017 Sean Wheeldon"
+description = "BrewLAN additional sorian AI support. Separated out for testing purposes."
+author = "Sean Wheeldon"
+icon = "/mods/BrewLAN_Modules/BrewAI/mod_icon.dds"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = {"25D57D85-7D84-27HT-A501-BR3WL4N000075",}
+requiresNames = {["25D57D85-7D84-27HT-A501-BR3WL4N000075"] = "BrewLAN",}
+conflicts = {  }
+before = {  }
+after = {  }
