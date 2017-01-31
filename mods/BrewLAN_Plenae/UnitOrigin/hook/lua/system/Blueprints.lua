0e0f79103d1bf1f47fe606de839ef18294c72b55
The-Balthazar <the.balthazar@gmail.com>
2017-01-31 17:35:34 +0000

    New meta-mod for giving attribution.

---
+++ mods/BrewLAN_Plenae/UnitOrigin/hook/lua/system/Blueprints.lua
@@ -0,0 +1,47 @@
+--------------------------------------------------------------------------------
+-- Hook File: /lua/system/blueprints.lua
+--------------------------------------------------------------------------------
+-- Modded By: Balthazar
+--------------------------------------------------------------------------------
+do
+
+local OldModBlueprints = ModBlueprints
+
+function ModBlueprints(all_blueprints)         
+    OldModBlueprints(all_blueprints)
+    
+    OriginAbility(all_blueprints.Unit)
+end
+
+--------------------------------------------------------------------------------
+-- Flagging units origin in abilities list.
+--------------------------------------------------------------------------------
+
+function OriginAbility(all_bps)
+    for id, bp in all_bps do
+        --If it's a selectable unit
+        if table.find(bp.Categories, 'SELECTABLE') then
+            local Source = bp.Source
+            if string.sub(Source, 1, 7) != "/units/" then
+                for i, mod in __active_mods do
+                    local dirlen = string.len(mod.location)
+                    if mod.location == string.sub(Source, 1, dirlen ) and string.sub(Source, dirlen + 1, dirlen + 1) == "/" then
+                        Source = mod.name
+                        if mod.author then
+                            Source = Source .. " (by " .. mod.author .. ")"
+                        end
+                        break
+                    end
+                end
+                if not bp.Display.Abilities then
+                    bp.Display.Abilities = {}
+                else
+                    table.insert(bp.Display.Abilities, "")
+                end
+                table.insert(bp.Display.Abilities, "Mod: " .. Source)
+            end
+        end
+    end
+end
+    
+end
---
+++ mods/BrewLAN_Plenae/UnitOrigin/mod_info.lua
@@ -0,0 +1,16 @@
+name = "Baristas"
+uid = "25D57D85-9JA7-D842-GKG4-ORIGIN0000001"
+version = 1
+copyright = "© 2017 Sean Wheeldon"
+description = "Lists each unit's source mod as one of its abilities."
+author = "Sean Wheeldon"
+icon = "/mods/BrewLAN_Plenae/UnitOrigin/mod_icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = {
+}before = { }
+after = { }
