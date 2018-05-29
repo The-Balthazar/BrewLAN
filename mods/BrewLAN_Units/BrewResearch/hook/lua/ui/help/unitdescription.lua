3979aa00b3c07a4676b02eafdb7470e4b909e25f
Balthazar <the.balthazar@Gmail.com>
2018-05-29 01:00:31 +0100

    Added descriptions to a bunch of planned units.

--- mods/BrewLAN_Units/BrewResearch/hook/lua/ui/help/unitdescription.lua
+++ mods/BrewLAN_Units/BrewResearch/hook/lua/ui/help/unitdescription.lua
@@ -1,3 +1,4 @@
+--Tech level research
 Description['sar9100'] = "<LOC Unit_Description_RND_001>Unlocks additional tech 1 units and unit research."
 Description['ser9100'] = "<LOC Unit_Description_RND_001>Unlocks additional tech 1 units and unit research."
 Description['srr9100'] = "<LOC Unit_Description_RND_001>Unlocks additional tech 1 units and unit research."
@@ -15,6 +16,7 @@ Description['ser9400'] = "<LOC Unit_Description_RND_004>Unlocks experimental uni
 Description['srr9400'] = "<LOC Unit_Description_RND_004>Unlocks experimental units and unit research."
 Description['ssr9400'] = "<LOC Unit_Description_RND_004>Unlocks experimental units and unit research."
 
+--Research stations
 Description['sab9101'] = "<LOC Unit_Description_RND_005>Basic research facility. Grants access to new units and higher tech levels."
 Description['seb9101'] = "<LOC Unit_Description_RND_005>Basic research facility. Grants access to new units and higher tech levels."
 Description['srb9101'] = "<LOC Unit_Description_RND_005>Basic research facility. Grants access to new units and higher tech levels."
@@ -28,19 +30,63 @@ Description['seb9301'] = "<LOC Unit_Description_RND_007>Advanced research facili
 Description['srb9301'] = "<LOC Unit_Description_RND_007>Advanced research facility. Has increased research speed and durability."
 Description['ssb9301'] = "<LOC Unit_Description_RND_007>Advanced research facility. Has increased research speed and durability."
 
-Description['sra0211'] = "<LOC Unit_Description_RND_008>Intermediate bomber aircraft."
-Description['sea0211'] = "<LOC Unit_Description_RND_009>Intermediate bomber aircraft."
-Description['ssa0211'] = "<LOC Unit_Description_RND_010>Intermediate bomber aircraft."
+--Unlockable factories
+Description['sab0105'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['seb0105'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['srb0105'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['ssb0105'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['sab0205'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['seb0205'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['srb0205'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['ssb0205'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['sab0305'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['seb0305'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['srb0305'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
+Description['ssb0305'] = "<LOC Unit_Description_RND_013>Produces amphibious units."
 
+Description['sab0106'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['seb0106'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['srb0106'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['ssb0106'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['sab0206'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['seb0206'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['srb0206'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['ssb0206'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['sab0306'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['seb0306'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['srb0306'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+Description['ssb0306'] = "<LOC Unit_Description_RND_014>Produces submersible units."
+
+Description['sab0107'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['seb0107'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['srb0107'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['ssb0107'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['sab0207'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['seb0207'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['srb0207'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['ssb0207'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['sab0307'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['seb0307'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['srb0307'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+Description['ssb0307'] = "<LOC Unit_Description_RND_015>Produces seaplanes"
+
+--Replacement units for research locked vanilla units
 Description['sab1101'] = "<LOC Unit_Description_RND_011>Produces a variable quantity of energy."
 Description['seb1101'] = "<LOC Unit_Description_RND_011>Produces a variable quantity of energy."
 Description['srb1101'] = "<LOC Unit_Description_RND_011>Produces a variable quantity of energy."
 Description['ssb1101'] = "<LOC Unit_Description_RND_011>Produces a variable quantity of energy."
 
+--Unlockable units
+Description['sra0211'] = "<LOC Unit_Description_RND_008>Intermediate bomber aircraft."
+Description['sea0211'] = "<LOC Unit_Description_RND_009>Intermediate bomber aircraft."
+Description['ssa0211'] = "<LOC Unit_Description_RND_010>Intermediate bomber aircraft."
+
 Description['sab3301'] = "<LOC Unit_Description_RND_012>Sensor array. Capable of identifying targets through stealth and cloak and clears jammer blips."
 Description['seb3301'] = "<LOC Unit_Description_RND_012>Sensor array. Capable of identifying targets through stealth and cloak and clears jammer blips."
 Description['srb3301'] = "<LOC Unit_Description_RND_012>Sensor array. Capable of identifying targets through stealth and cloak and clears jammer blips."
 Description['ssb3301'] = "<LOC Unit_Description_RND_012>Sensor array. Capable of identifying targets through stealth and cloak and clears jammer blips."
+
+--Description['something'] = "<LOC Unit_Description_RND_016>"
 
 do
     for id, bp in __blueprints do
