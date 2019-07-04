ade81ea711fcafa69109c296250e2fde3f3f7dd8
Balthazar <the.balthazar@Gmail.com>
2019-07-04 06:22:55 +0100

    Albatross redesign.

--- mods/BrewLAN/documentation/Changelog.md
+++ mods/BrewLAN/documentation/Changelog.md
@@ -445,6 +445,10 @@ All changes by Sean Wheeldon (Balthazar) unless otherwise stated.
 
   * Finished the rework of the Aeon Mobile Sensor.
 
+  ### 2019-07-04
+
+  * Finished the redesign of the Albatross.
+
 ## 2019-01-03|84 (FAF minor patch)
 
 * Created a function to add the FAF category CQUEMOV to function to mobile experimental mobile units.
--- mods/BrewLAN/units/SEA0307/SEA0307_script.lua
+++ mods/BrewLAN/units/SEA0307/SEA0307_script.lua
@@ -2,14 +2,38 @@
 --  Summary  :  UEF Torpedo Bomber Script
 --------------------------------------------------------------------------------
 local TAirUnit = import('/lua/terranunits.lua').TAirUnit
-local TANTorpedoAngler = import('/lua/terranweapons.lua').TANTorpedoAngler
-local TAirToAirLinkedRailgun = import('/lua/terranweapons.lua').TAirToAirLinkedRailgun
+local Twepins = import('/lua/terranweapons.lua')
+local TANTorpedoAngler = Twepins.TANTorpedoAngler
+local TAirToAirLinkedRailgun = Twepins.TAirToAirLinkedRailgun
 
 SEA0307 = Class(TAirUnit) {
     Weapons = {
         Torpedo = Class(TANTorpedoAngler) {},
         LinkedRailGun = Class(TAirToAirLinkedRailgun) {},
     },
+
+    OnStopBeingBuilt = function(self,builder,layer)
+        TAirUnit.OnStopBeingBuilt(self,builder,layer)
+        self.LandingAnimManip = CreateAnimator(self):SetPrecedence(0):PlayAnim(self:GetBlueprint().Display.AnimationLand):SetRate(1)
+    end,
+
+    OnMotionVertEventChange = function(self, new, old)
+        TAirUnit.OnMotionVertEventChange(self, new, old)
+        if new == 'Down' and self.LandingAnimManip then
+            self.LandingAnimManip:SetRate(-1)
+        elseif new == 'Up' or new == 'Top' and self.LandingAnimManip then
+            self.LandingAnimManip:SetRate(1)
+        end
+    end,
+
+    OnLayerChange = function(self, new, old)
+        TAirUnit.OnLayerChange(self, new, old)
+        if (new == 'Land' or new == 'Water') and self.LandingAnimManip then
+            self.LandingAnimManip:SetRate(-1)
+        elseif new == 'Air' and self.LandingAnimManip then
+            self.LandingAnimManip:SetRate(1)
+        end
+    end,
 }
 
 TypeClass = SEA0307
