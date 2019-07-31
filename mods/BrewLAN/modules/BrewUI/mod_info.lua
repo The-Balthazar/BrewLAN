9a3f2f77fcacbff26ac84e7abb6b0ef6c659021a
Balthazar <the.balthazar@Gmail.com>
2019-08-01 00:07:14 +0100

    Moved the BrewLAN modules inside BrewLAN itself, and retired the folder.

---
+++ mods/BrewLAN/modules/BrewUI/mod_info.lua
@@ -0,0 +1,14 @@
+name = "BrewLAN: UI"
+uid = "25D57D85-7D84-27HT-A501-BR3WUI0000003"
+version = 0.3
+copyright = "© 2009–2018 Sean Wheeldon"
+description = "BrewLAN engineer tab. Dormant without BrewLAN. May conflict with mods with a \"/hook/lua/ui/game/avatars.lua\" file while BrewLAN is active."
+author = "Balthazar"
+icon = "/mods/BrewLAN/modules/BrewUI/BrewUI.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+conflicts = {  }
+before = {  }
+after = {  }
