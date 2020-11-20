4cb7e01dc6e64c8f2218c629b5531c37cd1fedfb
Balthazar <the.balthazar@Gmail.com>
2020-11-20 18:13:28 +0000

    version number change for EIO missed from previous. What is --force lol

--- mods/BrewLAN_Plenae/ExperimentalIcons/mod_info.lua
+++ mods/BrewLAN_Plenae/ExperimentalIcons/mod_info.lua
@@ -1,7 +1,7 @@
 name = "Experimental Icons Overhaul"
-uid = "2529ea71-93ef-41a6-b552-EXPERICON00009"
-version = 0.9
-copyright = "© 2015–2018 Sean Wheeldon"
+uid = "2529ea71-93ef-41a6-b552-EXPERICON00010"
+version = 1
+copyright = "© 2015–2020 Sean Wheeldon"
 description = "If you can't see the mod icon then it can't detect the SCD file, and probably wont work. Your installation is likely wrong."
 author = "Balthazar"
 icon = "/mods/BrewLAN_Plenae/ExperimentalIcons/Experimental-Icon-Overhaul_Mod_Icon.dds"
