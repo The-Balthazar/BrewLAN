7d5c86cbd9b3851320f27bfde1324be575e8b792
Balthazar <the.balthazar@Gmail.com>
2022-05-29 00:17:28 +0100

    Updated CC mod info.

--- mods/BrewLAN_Units/BrewTurrets/mod_info.lua
+++ mods/BrewLAN_Units/BrewTurrets/mod_info.lua
@@ -1,7 +1,7 @@
 name = "BrewLAN: Caffe Corretto"
-uid = "25D57D85-9JA7-D842-BREW-TURRETS000003"
-version = 0.3
-copyright = "© 2017–2018 Sean Wheeldon"
+uid = "25D57D85-9JA7-D842-BREW-TURRETS000004"
+version = 0.4
+copyright = "© 2017–2022 Sean Wheeldon"
 description = "Coffee with a shot; adds turrets and buff nodes. All units built by BrewLAN field engineers."
 author = "Balthazar"
 icon = "/mods/BrewLAN_Units/BrewTurrets/mod_icon.png"
